8ab943b42bc85a7b1771fe504a89683ef0665bda
chinmayi1001 <127097611+chinmayi1001@users.noreply.github.com>
2023-11-01 22:20:33 +0530

    Update cppfile.cpp

--- cppfile.cpp
+++ cppfile.cpp
@@ -1,63 +1,56 @@
-// A Java solution for longest palindrome
+// A C++ solution for longest palindrome
 
-import java.util.*;
+#include <bits/stdc++.h>
+using namespace std;
 
-class GFG {
+// Function to print a substring str[low..high]
+void printSubStr(string str, int low, int high)
+{
+	for (int i = low; i <= high; ++i)
+		cout << str[i];
+}
 
-	// Function to print a subString str[low..high]
-	static void printSubStr(String str, int low, int high)
-	{
-		for (int i = low; i <= high; ++i)
-			System.out.print(str.charAt(i));
-	}
+// This function prints the longest palindrome substring
+// It also returns the length of the longest palindrome
+int longestPalSubstr(string str)
+{
+	// Get length of input string
+	int n = str.size();
 
-	// This function prints the
-	// longest palindrome subString
-	// It also returns the length
-	// of the longest palindrome
-	static int longestPalSubstr(String str)
-	{
-		// Get length of input String
-		int n = str.length();
+	// All substrings of length 1 are palindromes
+	int maxLength = 1, start = 0;
 
-		// All subStrings of length 1
-		// are palindromes
-		int maxLength = 1, start = 0;
+	// Nested loop to mark start and end index
+	for (int i = 0; i < str.length(); i++) {
+		for (int j = i; j < str.length(); j++) {
+			int flag = 1;
 
-		// Nested loop to mark start and end index
-		for (int i = 0; i < str.length(); i++) {
-			for (int j = i; j < str.length(); j++) {
-				int flag = 1;
+			// Check palindrome
+			for (int k = 0; k < (j - i + 1) / 2; k++)
+				if (str[i + k] != str[j - k])
+					flag = 0;
 
-				// Check palindrome
-				for (int k = 0; k < (j - i + 1) / 2; k++)
-					if (str.charAt(i + k)
-						!= str.charAt(j - k))
-						flag = 0;
-
-				// Palindrome
-				if (flag != 0 && (j - i + 1) > maxLength) {
-					start = i;
-					maxLength = j - i + 1;
-				}
+			// Palindrome
+			if (flag && (j - i + 1) > maxLength) {
+				start = i;
+				maxLength = j - i + 1;
 			}
 		}
-
-		System.out.print(
-			"Longest palindrome substring is: ");
-		printSubStr(str, start, start + maxLength - 1);
-
-		// Return length of LPS
-		return maxLength;
 	}
 
-	// Driver Code
-	public static void main(String[] args)
-	{
-		String str = "forgeeksskeegfor";
-		System.out.print("\nLength is: "
-						+ longestPalSubstr(str));
-	}
+	cout << "Longest palindrome substring is: ";
+	printSubStr(str, start, start + maxLength - 1);
+
+	// Return length of LPS
+	return maxLength;
+}
+
+// Driver Code
+int main()
+{
+	string str = "forgeeksskeegfor";
+	cout << "\nLength is: " << longestPalSubstr(str);
+	return 0;
 }
 
 // This code is contributed by shikhasingrajput
